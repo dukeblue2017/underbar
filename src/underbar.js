292239b5eef0ade4b93b4f9991e92828b7367324
Carter Duncan <dukeblue2017@gmail.com>
2017-09-19 19:28:31 -0400

    finished 'defaults'

--- src/underbar.js
+++ src/underbar.js
@@ -259,12 +259,29 @@
   //   }, {
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
-  _.extend = function(obj) {
+  _.extend = function(obj, src) {
+    for (var i = 0; i < arguments.length; i++) {
+      var src = arguments[i];
+      for (var item in src) {
+        obj[item] = src[item];
+      }
+    } return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    for (var i = 0; i < arguments.length; i++) {
+      var src = arguments[i];
+      for (var item in src) {
+        console.log(obj)
+        if (item in obj) {
+
+        } else {
+          obj[item] = src[item];
+        }
+      }
+    } return obj;
   };
 
 
